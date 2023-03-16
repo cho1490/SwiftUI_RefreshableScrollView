2d4e97d2fce9d563ed6f7dea2197ebb2f44dd90f
조상현 <chosanghyun99@gmail.com>
2023-03-16 14:06:14 +0900

    Initial Commit

---
+++ SwiftUIInfinityScrollView/SwiftUIInfinityScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIInfinityScrollViewApp.swift
+//  SwiftUIInfinityScrollView
+//
+//  Created by 조상현 on 2023/03/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIInfinityScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
